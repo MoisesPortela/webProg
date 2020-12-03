5f2ef00c2467ce268797d8f9fb7e2699768b070c
Sander Rodrigues <sander.rodriguesr75@gmail.com>
2020-12-03 23:27:17 +0000

    Upload New File

---
+++ Projeto/Programas/classes/estoque.py
@@ -0,0 +1,17 @@
+from BDDS.controllers.estoqueController import estoqueController
+
+
+
+class Estoque():
+
+    def __init__(self, idtSangue, tipo, rh, qtdDisponivelLitros):
+        self.idtSangue = idtSangue
+        self.tipo = tipo
+        self.rh = rh
+        self.qtdDisponivelLitros = qtdDisponivelLitros
+        
+        
+    
+
+
+
